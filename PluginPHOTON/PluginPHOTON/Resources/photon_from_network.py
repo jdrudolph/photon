ac29a90b38f5b862b43a63994c8d19f5e8a3c9ee
Jan Rudolph <rudolph@biochem.mpg.de>
2017-10-17 16:40:09 +0200

    run photon from annotated network

---
+++ PluginPHOTON/PluginPHOTON/Resources/photon_from_network.py
@@ -0,0 +1,135 @@
+import sys
+from itertools import takewhile
+from perseuspy import pd, nx, write_networks, read_networks
+import numpy as np
+import perseuspy.parameters as params
+import phos.data.network as networker
+import phos.algo.anat as anat
+import phos.algo.activity as activity
+
+
+def split_and_stack(frame, colname):
+    mask = frame[colname].isnull()
+    masked_frame=frame[~mask]
+    mapping = pd.DataFrame([{i: v for i,v in enumerate(x)} for x in masked_frame[colname]],
+                             index=masked_frame.index).stack().reset_index(1,drop=True)
+    mapping.name = colname
+    return frame.drop(colname, axis=1).join(mapping)
+
+def aggregate_scores(scores):
+    """Aggregate all scores into one table.
+    :param scores: list of pd.DataFrame with scores and additional 'Column Name' column
+    """
+    _score = pd.concat(scores)
+    score = (_score.pivot_table('score_empiric', 'GeneID', 'Column Name')
+            .rename(columns = lambda x: '{} Signaling score'.format(x)))
+    for num_column in ['t', 'p_greater', 'p_lesser', 'p_twosided', 'q_greater', 'q_lesser', 'q_twosided']:
+        values = (_score
+                .pivot_table(num_column, 'GeneID', 'Column Name')
+                .rename(columns = lambda x : '{} {}'.format(x, num_column))
+                .apply(lambda col: col.astype(float)))
+        result = score.join(values)
+    for cat_column in ['rej_greater', 'rej_lesser', 'rej_twosided', 'Significant']:
+        significant = (_score
+                .pivot_table(cat_column, 'GeneID', 'Column Name')
+                .rename(columns = lambda x : '{} {}'.format(x, cat_column))
+                .fillna('Insufficient data')
+                .apply(lambda col: col.astype('category')))
+        score = score.join(significant)
+    score = score.reset_index().rename(columns = {"GeneID": "Node"})
+    return score
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='PHOTON: PHOsphoproteomic dissecTiOn using Networks')
+    parser.add_argument('paramfile', type=argparse.FileType('r'))
+    parser.add_argument('infolder', type=str)
+    parser.add_argument('outfolder', type=str)
+    parser.add_argument('subnetworks', type=str)
+    parser.add_argument('signaling_scores', type=argparse.FileType('w'))
+    args = parser.parse_args()
+    print('reading parameters')
+    paramFile = params.parse_parameters(args.paramfile)
+    parameters = {
+            'activity' : {
+                'min_size' : params.intParam(paramFile, 'Required number of observations'),
+                'permutations' : params.intParam(paramFile, 'Number of permutations')
+                }}
+    data_columns = params.multiChoiceParam(paramFile, 'Data columns')
+    if len(data_columns) < 1:
+        print("Please choose at least one data column")
+        sys.exit(1)
+    confidence_column = params.singleChoiceParam(paramFile, 'Confidence column')
+    if type(confidence_column) is int and confidence_column < 0:
+        print("Confidence column was not chosen")
+        sys.exit(1)
+    anchor = params.stringParam(paramFile, 'Signaling source')
+    if anchor is not None:
+        try:
+            anchor = int(anchor)
+        except ValueError:
+            print("Cannot convert signaling source {} to entrez gene id. Please enter a number, or leave blank".format(anchor))
+            sys.exit(1)
+    networks_table, networks = read_networks(args.infolder)
+    for guid in networks_table['GUID']:
+        name, node_table, edge_table = [networks[guid][key] for key in ['name', 'node_table', 'edge_table']]
+        scores = []
+        subnets = []
+        graphs = []
+        terminals = []
+        for data_column in data_columns:
+            print('calculating scores for', data_column)
+            if 'GeneID' in node_table.columns:
+                __exp = node_table.drop('GeneID', 1)
+            else:
+                __exp = node_table
+            if data_column != 'avg' and 'avg' in node_table.columns:
+                __exp = __exp.drop('avg', 1)
+            _exp = __exp.rename(columns = {'Node': 'GeneID', data_column: 'avg'})[['GeneID', 'avg']]
+            exp = split_and_stack(_exp, 'avg').dropna()
+            network = edge_table.rename(columns = {'Source': 'kin', 'Target': 'sub', confidence_column: 'confidence'})
+            score = activity.empiric(exp, network, **parameters['activity']).assign(**{'Column Name': data_column})
+            scores.append(score)
+            terminal = score[score['Significant']]['GeneID'].astype(str)
+            terminals.append(terminal.to_frame().assign(**{'Column Name': data_column}))
+            network_undirected = network[network['kin'] < network['sub']]
+            subnet = anat.remote_network('Perseus', network_undirected, terminal, anchor = anchor)
+            G = nx.from_pandas_dataframe(subnet, 's', 't', edge_attr=True)
+            G.graph['Name'] = data_column
+            for node in G:
+                if str(node) in set(terminal.astype(str)):
+                    node_type = 'terminal'
+                elif str(node) == str(anchor):
+                    node_type = 'anchor'
+                else:
+                    node_type = 'connector'
+                G.node[node]['Type'] = node_type
+            graphs.append(G)
+            subnet['Column Name'] = data_column
+            subnets.append(subnet)
+        # aggregate scores
+        score = aggregate_scores(scores)
+        score.to_perseus(args.signaling_scores, main_columns = ['{} Signaling score'.format(x) for x in data_columns])
+        # aggregate networks
+        subnetworks_table, subnetworks = nx.to_perseus(graphs)
+        write_networks(args.subnetworks, subnetworks_table, subnetworks)
+        subnet = pd.concat(subnets)
+        # annotate input network
+        connector = pd.concat([subnet[['s', 'Column Name']].rename(columns={'s': 'Node'}),
+            subnet[['t', 'Column Name']].rename(columns={'t': 'Node'})]).astype(str)
+        terminal = pd.concat(terminals).rename(columns={'GeneID': 'Node'})
+        _functions = pd.concat([terminal.assign(Function='Terminal'),
+                               connector.assign(Function='Connector')])
+        if anchor is not None:
+            _functions = _functions.append(pd.DataFrame({'Node':anchor, 'Column Name':data_columns, 'Function':'Anchor'}))
+        functions = (_functions.groupby(['Node', 'Column Name'])['Function']
+                .unique().str.join(';').unstack().apply(lambda col: col.astype('category'))
+                .rename(columns = lambda col: '{} Function'.format(col)).reset_index())
+        networks[guid]['node_table'] = node_table.merge(score, how='left').merge(functions, how='left')
+        edges = (pd.concat([subnet, subnet.rename({'s':'t', 't':'s'})])
+                .groupby(['s', 't'])['Column Name']
+                .unique().str.join(';').astype('category').reset_index()
+                .rename(columns={'s' : 'Source', 't': 'Target', 'Column Name': 'Reconstructed networks'}))
+        networks[guid]['edge_table'] = edge_table.merge(edges, how='left')
+    print('writing networks')
+    write_networks(args.outfolder, networks_table, networks)
