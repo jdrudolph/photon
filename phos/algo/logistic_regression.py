1ca29a98b936052aaa89cd489355f14b22c508f4
Jan <rudolph@biochem.mpg.de>
2018-11-19 18:14:17 +0100

    add support for sklearn >= 0.20

--- phos/algo/logistic_regression.py
+++ phos/algo/logistic_regression.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import scipy
 from sklearn import metrics
-from sklearn import cross_validation as cv
+from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LogisticRegression
 
 
@@ -19,19 +19,19 @@ def logistic_regression(df, features, N=10):
     X = df[features].values
     y = df['label'].values
     classifier = LogisticRegression(class_weight='balanced')
-    kfold = cv.StratifiedKFold(y, n_folds=N)
+    kfold = StratifiedKFold(n_splits=N)
     mean_tpr = 0.0
     mean_fpr = np.linspace(0, 1, 100)
     all_probas = np.zeros_like(y, dtype=float)
     all_coef = []
-    for i, (train, test) in enumerate(kfold):
+    for train, test in kfold.split(X, y):
         probas = classifier.fit(X[train], y[train]).predict_proba(X[test])
         all_probas[test] = probas[:,1]
         all_coef.append(classifier.coef_[0])
         fpr, tpr, _ = metrics.roc_curve(y[test], probas[:, 1])
         mean_tpr += scipy.interp(mean_fpr, fpr, tpr)
         mean_tpr[0] = 0.0
-    mean_tpr = mean_tpr / len(kfold)
+    mean_tpr = mean_tpr / N
     mean_tpr[-1] = 1.0
     mean_auc = metrics.auc(mean_fpr, mean_tpr)
     coef = pd.DataFrame(all_coef, columns=features).mean()
